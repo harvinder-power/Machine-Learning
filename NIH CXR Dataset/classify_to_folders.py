fc6c310676616ce3459906322d89996ae1bad83a
Harvinder Power <harvinder.power@gmail.com>
2017-11-27 20:48:30 +0000

    Completed system to classify images to seperate folders

--- NIH CXR Dataset/classify_to_folders.py
+++ NIH CXR Dataset/classify_to_folders.py
@@ -2,6 +2,16 @@ import pandas as pd
 import numpy as np
 import os
 import time
+import csv
 
-classifiers = pd.read_csv('classifications.csv')
-print classifiers.head()
+#classifiers = pd.read_csv('classifications.csv')
+with open('classifications.csv', 'rb') as f:
+    reader = csv.reader(f)
+    my_list = list(reader)
+print my_list[1]
+
+count = 0
+for i in xrange(len(my_list)-1):
+    if my_list[i+1][1] == "Cardiomegaly":
+         count+=1
+    print count
---
+++ NIH CXR Dataset/seperate_by_classifier.py
@@ -0,0 +1,20 @@
+import csv
+
+with open('/Users/harvinderpower/Desktop/test.csv', 'rb') as f:
+    reader = csv.reader(f)
+    my_list = list(reader)
+print my_list[1]
+
+count = 0
+classifiers = []
+
+for i in xrange(len(my_list)-1):
+    if (my_list[i][0] in classifiers):
+         pass
+    else:
+        classifiers.append(my_list[i][0])
+print(classifiers)
+
+with open('determined_classifiers.csv', 'wb') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(classifiers)
