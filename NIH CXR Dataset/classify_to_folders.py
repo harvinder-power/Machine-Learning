eaac904ea2dc15a4af2dd629ade2ec49e7078785
Harvinder Power <harvinder.power@gmail.com>
2017-11-28 16:40:07 +0000

    Now gives names of each file which meets the criteria

--- NIH CXR Dataset/classify_to_folders.py
+++ NIH CXR Dataset/classify_to_folders.py
@@ -10,8 +10,11 @@ with open('classifications.csv', 'rb') as f:
     my_list = list(reader)
 print my_list[1]
 
+names = []
 count = 0
 for i in xrange(len(my_list)-1):
     if my_list[i+1][1] == "Cardiomegaly":
          count+=1
+         names.append(my_list[i+1][0])
     print count
+print names
