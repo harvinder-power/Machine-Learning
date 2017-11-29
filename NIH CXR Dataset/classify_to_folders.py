f531e56b42efbddc1487e744bc45a4d0ddff396c
Harvinder Power <harvinder.power@gmail.com>
2017-11-29 16:00:59 +0000

    Added labels into the sorting system

--- NIH CXR Dataset/classify_to_folders.py
+++ NIH CXR Dataset/classify_to_folders.py
@@ -4,17 +4,26 @@ import os
 import time
 import csv
 
-#classifiers = pd.read_csv('classifications.csv')
+
+#Open classifications folder - and import data
 with open('classifications.csv', 'rb') as f:
     reader = csv.reader(f)
     my_list = list(reader)
-print my_list[1]
 
-names = []
+#Open determined_classifiers and add labels to a list called "labels"
+with open('determined_classifiers.csv', 'rb') as f:
+    reader = csv.reader(f)
+    labels = list(reader)
+print "Labels:", labels
+
+
+for x in labels:
+    i
 count = 0
-for i in xrange(len(my_list)-1):
-    if my_list[i+1][1] == "Cardiomegaly":
-         count+=1
-         names.append(my_list[i+1][0])
-    print count
-print names
+for x in labels:
+    for i in xrange(len(my_list)-1):
+        if my_list[i+1][1] == "Cardiomegaly":
+             count+=1
+             names.append(my_list[i+1][0])
+        print count
+print "Cardiomegaly images", names
